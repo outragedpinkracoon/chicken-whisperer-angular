3ead9e945fddeb166c532d08e3b9279a6b7e612a
Valerie Gibson <vgibson14@gmail.com>
2016-05-04 21:33:37 +0100

    capture spec amagad

--- app/ts/game/capture.spec.ts
+++ app/ts/game/capture.spec.ts
@@ -2,7 +2,7 @@ import {Capture} from './capture';
 import {Die} from './die';
 import {Player} from './player';
 import {ChickenPen} from './chickenPen';
-import { CHICKENS } from './mock-chickens';
+import {Chicken} from './chicken';
 
 describe("Capture", function(){
 
@@ -14,7 +14,12 @@ describe("Capture", function(){
 
   beforeEach(function(){
 
-   chickenPen = new ChickenPen(CHICKENS)
+   chickenPen = new ChickenPen([
+      new Chicken({ "id": 1, "name": "Popo", "speed": 14, "maxScare": 1 }),
+      new Chicken({ "id": 2, "name": "Pudgy", "speed": 5, "maxScare": 3 }),
+      new Chicken({ "id": 3, "name": "Jojo", "speed": 8, "maxScare": 3 }),
+      new Chicken({ "id": 4, "name": "Colin", "speed": 10, "maxScare": 2 })
+    ]);
     var captureOptions = {
       chickenPen: chickenPen, 
       die: new Die()
@@ -43,7 +48,7 @@ describe("Capture", function(){
   });
   
   it("should be a successful roll", function(){
-    var result = capture.successfulRoll([5,5], chickenPen.chickens[0]);
+    var result = capture.successfulRoll([5,10], chickenPen.chickens[0]);
     expect(result).toBe(true);
   });
 
@@ -52,55 +57,55 @@ describe("Capture", function(){
     expect(result).toBe(false);
   });
 
-  // it("should reduce chicken's speed on failure", function(){
-  //   capture.die = fakeDie;
-  //   var result = capture.failure(chickenPen.chickens[0]);
+  it("should reduce chicken's speed on failure", function(){
+    capture.die = fakeDie;
+    var result = capture.failure(chickenPen.chickens[0]);
     
-  //   expect(result).toBe(false);
-  //   expect(capture.chickenPen.chickens[0].speed).toBe(4);
-  // });
+    expect(result).toBe(false);
+    expect(chickenPen.chickens[0].speed).toBe(13);
+  });
 
 
-  // it("should invoke failure", function(){
-  //   capture.die = fakeDie;
-
-  //   var result = capture.attempt(player, chickenPen.chickens[0], chickenPen, 1);
+  it("should invoke failure", function(){
+    spyOn(capture,"failure");
     
-  //   expect(capture.failure.calls.count()).toBe(1);
-  // });
+    var result = capture.attempt(player, chickenPen.chickens[0], chickenPen, 1);
+    
+    expect(capture.failure.calls.count()).toBe(1);
+  });
 
-  // it("should not alter chicken speed on success", function(){
-  //   var result = capture.success(chickenPen, player, chickenPen.chickens[0]);
+  it("should not alter chicken speed on success", function(){
+    var result = capture.success(chickenPen, player, chickenPen.chickens[0]);
 
-  //   expect(result).toBe(true);
-  //   expect(chickenPen.chickens[0].speed).toBe(5);
-  // });
+    expect(result).toBe(true);
+    expect(chickenPen.chickens[0].speed).toBe(5);
+  });
 
-  // it("should add chicken to player on success", function(){
-  //   spyOn(capture.die, "roll").and.returnValue(10);
-  //   var chicken = chickenPen.chickens[0];
-  //   var result = capture.success(chickenPen, player, chickenPen.chickens[0]);
+  it("should add chicken to player on success", function(){
+    spyOn(capture.die, "roll").and.returnValue(10);
+    var chicken = chickenPen.chickens[0];
+    var result = capture.success(chickenPen, player, chickenPen.chickens[0]);
 
-  //   expect(result).toBe(true);
-  //   expect(player.chickens[0]).toEqual(chicken);
-  // });
+    expect(result).toBe(true);
+    expect(player.chickens[0]).toEqual(chicken);
+  });
 
-  // it("should remove chicken from chickenPen on success", function(){
-  //   spyOn(capture.die, "roll").and.returnValue(10);
-  //   var chicken = chickenPen.chickens[0];
-  //   var result = capture.success(chickenPen, player, chicken);
+  it("should remove chicken from chickenPen on success", function(){
+    spyOn(capture.die, "roll").and.returnValue(10);
+    var chicken = chickenPen.chickens[0];
+    var result = capture.success(chickenPen, player, chicken);
 
-  //   expect(result).toBe(true);
-  //   expect(chickenPen.count()).toEqual(3);
-  // });
+    expect(result).toBe(true);
+    expect(chickenPen.count()).toEqual(3);
+  });
 
-  // it("should invoke success", function(){
-  //   spyOn(capture.die, "roll").and.returnValue(10);
-  //   spyOn(capture, "success");
-  //   var chicken = chickenPen.chickens[0];
-  //   var result = capture.attempt(player, chicken, chickenPen, 1);
+  it("should invoke success", function(){
+    spyOn(capture.die, "roll").and.returnValue(16);
+    spyOn(capture, "success");
+    var chicken = chickenPen.chickens[0];
+    var result = capture.attempt(player, chicken, chickenPen, 1);
 
-  //   expect(capture.success).toHaveBeenCalled();
-  // });
+    expect(capture.success).toHaveBeenCalled();
+  });
 
 })
