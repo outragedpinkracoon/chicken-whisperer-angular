6a079901bcee2111a85fb71230648f04e5e12d13
Valerie Gibson <vgibson14@gmail.com>
2016-05-04 21:23:14 +0100

    refactor

--- app/js/chickens/services/chicken.service.js
+++ app/js/chickens/services/chicken.service.js
@@ -1,4 +1,4 @@
-System.register(['../../game/mock-chickens', 'angular2/core'], function(exports_1, context_1) {
+System.register(['../../game/chickenPen', '../../game/chicken', 'angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,12 +10,15 @@ System.register(['../../game/mock-chickens', 'angular2/core'], function(exports_
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var mock_chickens_1, core_1;
+    var chickenPen_1, chicken_1, core_1;
     var ChickenService;
     return {
         setters:[
-            function (mock_chickens_1_1) {
-                mock_chickens_1 = mock_chickens_1_1;
+            function (chickenPen_1_1) {
+                chickenPen_1 = chickenPen_1_1;
+            },
+            function (chicken_1_1) {
+                chicken_1 = chicken_1_1;
             },
             function (core_1_1) {
                 core_1 = core_1_1;
@@ -25,10 +28,15 @@ System.register(['../../game/mock-chickens', 'angular2/core'], function(exports_
                 function ChickenService() {
                 }
                 ChickenService.prototype.getChickens = function () {
-                    return Promise.resolve(mock_chickens_1.CHICKENS);
+                    return Promise.resolve(new chickenPen_1.ChickenPen([
+                        new chicken_1.Chicken({ "id": 1, "name": "Popo", "speed": 14, "maxScare": 1 }),
+                        new chicken_1.Chicken({ "id": 2, "name": "Pudgy", "speed": 5, "maxScare": 3 }),
+                        new chicken_1.Chicken({ "id": 3, "name": "Jojo", "speed": 8, "maxScare": 3 }),
+                        new chicken_1.Chicken({ "id": 4, "name": "Colin", "speed": 10, "maxScare": 2 })
+                    ]));
                 };
                 ChickenService.prototype.getChicken = function (id) {
-                    return Promise.resolve(mock_chickens_1.CHICKENS).then(function (chickens) { return chickens.filter(function (chicken) { return chicken.id === id; })[0]; });
+                    return Promise.resolve(this.getChickens()).then(function (chickenPen) { return chickenPen.chickens.filter(function (chicken) { return chicken.id === id; })[0]; });
                 };
                 ChickenService = __decorate([
                     core_1.Injectable(), 
