e805a6b0f178805f582f8bf4628655b001de4303
Valerie Gibson <vgibson14@gmail.com>
2016-05-18 18:02:56 +0300

    revert

--- app/ts/race-game.component.ts
+++ app/ts/race-game.component.ts
@@ -1,17 +1,36 @@
 import {Component, Input} from 'angular2/core';
 import {RaceGame} from './game/raceGame';
+import { DiceService } from './services/diceService';
 import { RacePositionsComponent } from './race-positions.component';
-import { RaceResultsComponent } from './race-results.component';
+import { DiceResultsContainer } from './models/diceResultsContainer';
 
 @Component({
     selector: 'race-game',
     templateUrl: 'app/views/race-game/race-game.component.html',
+    providers: [DiceResultsContainer],
     styleUrls: ['app/views/race-game/race-game.component.css'],
-    directives: [RacePositionsComponent, RaceResultsComponent]
+    directives: [RacePositionsComponent]
 })
 
 export class RaceGameComponent {
   @Input('game') game: RaceGame;
+  success: boolean;
+  diceResults: Array<string>;
+  total:number;
+  moved: boolean;
+
+  constructor(private diceResultsContainer: DiceResultsContainer) {
+    this.diceResultsContainer = diceResultsContainer;
+  }
+
+  roll(){
+    if(this.game.isWon()) return;
+    this.success = this.game.roll();
+    this.diceResultsContainer.setupDiceResults(this.game.lastRolls,this.success);
+    
+    this.game.nextTurn();
+    this.moved = true;
+  }
 }
 
 
