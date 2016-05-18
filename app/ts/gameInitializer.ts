b5412f8b2ac967cecc0e358a8c15fcf6b694bf52
Valerie Gibson <vgibson14@gmail.com>
2016-05-18 21:57:31 +0300

    adds missing comma whoops

--- app/ts/gameInitializer.ts
+++ app/ts/gameInitializer.ts
@@ -20,7 +20,7 @@ export class GameInitializer {
       new Chicken({ "id": 1, "name": "Popo", "speed": 14, "maxScare": 1,
         "image":"/app/assets/images/chicken1.png" }),
       new Chicken({ "id": 2, "name": "Pudgy", "speed": 5, "maxScare": 3,
-        "image":"/app/assets/images/chicken2.png" })
+        "image":"/app/assets/images/chicken2.png" }),
       new Chicken({ "id": 3, "name": "Jojo", "speed": 8, "maxScare": 3,
         "image":"/app/assets/images/chicken3.png" }),
       new Chicken({ "id": 4, "name": "Colin", "speed": 10, "maxScare": 2,
