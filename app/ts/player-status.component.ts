0446b770569e810dd48cda207c8361dc883187b1
Valerie Gibson <vgibson14@gmail.com>
2016-05-28 20:36:24 +0100

    adds panda to player status

--- app/ts/player-status.component.ts
+++ app/ts/player-status.component.ts
@@ -1,14 +1,22 @@
 import {Component, Input} from 'angular2/core';
 import {CaptureGame} from './game/captureGame';
+import {AnimalInfo} from './models/animalInfo';
 
 @Component({
     selector: 'player-status',
     templateUrl: 'app/views/player-status/player-status.component.html',
-    styleUrls: ['app/views/player-status/player-status.component.css']
+    styleUrls: ['app/views/player-status/player-status.component.css'],
+    providers: [AnimalInfo]
 })
 
 export class PlayerStatusComponent {
   @Input('game') game: CaptureGame;
+  
+  info: AnimalInfo;
+
+  constructor(private animalInfo:AnimalInfo){
+    this.info = animalInfo;
+  }
 }
 
 
